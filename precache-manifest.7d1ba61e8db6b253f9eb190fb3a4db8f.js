0b098638490a18497007e18031e138a257033e07
dainryoo <dainryoo@ibm.com>
2020-10-05 10:20:47 -0500

    Updates

---
+++ precache-manifest.7d1ba61e8db6b253f9eb190fb3a4db8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40a0e83627d96e625c9b05b77d50779",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "8894d02a7cd97ac17d40",
+    "url": "/recipes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5421eebcb2ff151d3999",
+    "url": "/recipes/static/js/2.da2f9a26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.da2f9a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8894d02a7cd97ac17d40",
+    "url": "/recipes/static/js/main.4a975414.chunk.js"
+  },
+  {
+    "revision": "65aed4a3aaa500d440aa",
+    "url": "/recipes/static/js/runtime-main.9fddb04f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipes/static/media/logo.5d5d9eef.svg"
+  }
+]);
