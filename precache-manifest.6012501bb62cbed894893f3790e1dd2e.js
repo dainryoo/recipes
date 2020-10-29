6f5bece2b91f02569434e6a375caa46b7ca1bc74
dainryoo <dainryoo@ibm.com>
2020-10-29 14:21:00 -0500

    Updates

--- precache-manifest.6012501bb62cbed894893f3790e1dd2e.js
+++ precache-manifest.6012501bb62cbed894893f3790e1dd2e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8c5607a7cfad658feab807381d4a1be",
+    "revision": "9cc02fc716ba98046bcc0ffa5f1fb051",
     "url": "/recipes/index.html"
   },
   {
-    "revision": "840a783d46d1f074cc7b",
+    "revision": "7181000282e4f61d7f28",
     "url": "/recipes/static/css/main.7529b874.chunk.css"
   },
   {
-    "revision": "9227253d9de561b6540b",
-    "url": "/recipes/static/js/2.9beb3602.chunk.js"
+    "revision": "e9b783d81ac3db401a79",
+    "url": "/recipes/static/js/2.f19a0d8f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/recipes/static/js/2.9beb3602.chunk.js.LICENSE.txt"
+    "url": "/recipes/static/js/2.f19a0d8f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "840a783d46d1f074cc7b",
-    "url": "/recipes/static/js/main.e9e314ae.chunk.js"
+    "revision": "7181000282e4f61d7f28",
+    "url": "/recipes/static/js/main.af4cf97b.chunk.js"
   },
   {
     "revision": "65aed4a3aaa500d440aa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/recipes/precache-manifest.78591628b80f7d396c57ebf359f009eb.js"
+  "/recipes/precache-manifest.6012501bb62cbed894893f3790e1dd2e.js"
 );
 
 self.addEventListener('message', (event) => {
